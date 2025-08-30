b1ec21f09216dc338a4e1d357c92c5b322db8946
xiaobai-cn21 <josue2025cn@outlook.com>
2025-08-30 09:23:04 +0800

    sql表和auth文件

---
+++ server/auth.h
@@ -0,0 +1,9 @@
+#ifndef AUTH_H
+#define AUTH_H
+
+#include <string>
+
+
+bool authenticateUser(const std::string& username, const std::string& password);
+
+#endif 
---
+++ server/table.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE dongRuanSystem;
+
+USE dongRuanSystem
+
+CREATE TABLE users{
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin', 'factory', 'expert') NOT NULL
+};
+
+INSERT INTO users(username, password,role) 
+VALUES
+    ('factory1', 'woxihuanxian123', 'factory'),
+    ('expert1', 'beijingtairele123', 'expert'),
+    ('admin1', 'iamadmin123', 'admin');
