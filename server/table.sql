9b9ef6296c01ade01ef6684eb9cb1c90417dd0e1
xiaobai-cn21 <josue2025cn@outlook.com>
2025-08-30 10:12:27 +0800

    加了auditor角色

--- server/table.sql
+++ server/table.sql
@@ -6,7 +6,7 @@ CREATE TABLE users{
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    role ENUM('admin', 'factory', 'expert') NOT NULL
+    role ENUM('admin', 'factory', 'expert', 'auditor') NOT NULL
 };
 
 INSERT INTO users(username, password,role) 
