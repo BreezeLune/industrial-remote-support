aee854c46bc443d9212b5a07db3e5d973d3e1439
xiaobaicn-21 <josue2025cn@outlook.com>
2025-08-31 13:55:52 +0800

    added register system

--- server/auth.h
+++ server/auth.h
@@ -6,6 +6,6 @@
 
 AuthResult authenticateUser(const std::string& username, const std::string& password);
 AuthResult validateToken(const std::string& token);
-
-
+AuthResult registerUser(const std::string& username, const std::string& password, const std::string& email, const std::string& company, const std::string& requestedRole);
+bool isUsernameExists(const std::string& username);
 #endif 
